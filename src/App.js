237aea9a765aece8651233d71b1232b03d6bb142
Piyush Anand <piyushanand.1221@gmail.com>
2021-01-16 01:12:20 +0530

    logout, password reset and profile update ready

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import ForgetPassword from "./components/ForgetPassword";
 import Login from "./components/Login";
 import PrivateRoute from "./components/PrivateRoute";
 import Signup from "./components/Signup";
+import UpdateProfile from "./components/UpdateProfile";
 import { AuthProvider } from "./config/authContext";
 
 class App extends Component {
@@ -16,8 +18,13 @@ class App extends Component {
             <AuthProvider>
               <Switch>
                 <PrivateRoute exact path="/" component={Dashboard} />
+                <PrivateRoute
+                  path="/update-profile"
+                  component={UpdateProfile}
+                />
                 <Route path="/signup" component={Signup} />
                 <Route path="/login" component={Login} />
+                <Route path="/forget-password" component={ForgetPassword} />
               </Switch>
             </AuthProvider>
           </Router>
--- src/components/ForgetPassword.js
+++ src/components/ForgetPassword.js
@@ -1,26 +1,27 @@
 import React, { useRef, useState } from "react";
 import { Form, Button, Card, Alert } from "react-bootstrap";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useAuth } from "../config/authContext";
 
-export default function Login() {
+export default function ForgetPassword() {
   const emailRef = useRef();
-  const passwordRef = useRef();
-  const { login } = useAuth();
+  const { resetPassword } = useAuth();
   const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+
   const [loading, setLoading] = useState(false);
-  const history = useHistory();
 
   async function handleSubmit(e) {
     e.preventDefault();
 
     try {
+      setMessage("");
       setError("");
       setLoading(true);
-      await login(emailRef.current.value, passwordRef.current.value);
-      history.push("/");
+      await resetPassword(emailRef.current.value);
+      setMessage("Check Your Inbox for further instruction");
     } catch {
-      setError("Failed to Sign In");
+      setError("Failed to Reset Password");
     }
 
     setLoading(false);
@@ -30,23 +31,20 @@ export default function Login() {
     <>
       <Card style={{ background: "none" }}>
         <Card.Body>
-          <h2 className="text-center mb-4">Login</h2>
+          <h2 className="text-center mb-4">Password Reset</h2>
           {error && <Alert variant="danger">{error}</Alert>}
+          {message && <Alert variant="success">{message}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
             </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
             <Button disabled={loading} className="w-100" type="submit">
-              Log In
+              Reset Password
             </Button>
           </Form>
           <div className="w-100 text-center mt-3">
-            <Link to="/forget-password">Forget Password?</Link>
+            <Link to="/login">Login</Link>
           </div>
         </Card.Body>
       </Card>
